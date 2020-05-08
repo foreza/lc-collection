f73f5a665d127fdb5b4a647ace7e7d3fbeba0d88
Doki <jforeza@gmail.com>
2020-05-07 22:28:28 -0700

    Add solution for 14-longest-common-prefix
    Doing a nested loop (outer loop iterating on the first word, inner loop iterating on the array) works to give a worst case time complexity of essentially O(N*N)

--- lc-easy/14-longest-common-prefix/solution.js
+++ lc-easy/14-longest-common-prefix/solution.js
@@ -4,4 +4,34 @@
  */
 var longestCommonPrefix = function(strs) {
     
+    // Handle zero case. If no strings, no prefix
+    if (strs == undefined || strs.length == 0 ){
+        return "";
+    }
+
+    // If there's only one word, return that word
+    if (strs.length == 1) {
+        return strs[0];
+    }
+
+
+    let prefix = "";
+    
+    for (var i = 0; i < strs[0].length; ++i){
+            for (var j = 1; j < strs.length; ++j) {
+
+            if (strs[j][i] != strs[0][i]){
+                // The second we get a mismatch, return the prefix
+                return prefix;
+            }
+
+
+        }
+
+        // Only append the letter if all the letters pass
+        prefix += strs[0][i];
+    }
+
+    return prefix;
+
 };
