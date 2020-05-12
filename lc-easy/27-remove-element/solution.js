e838f0ab0cb7b12e46969bed465dec02b5f5b038
Doki <jforeza@gmail.com>
2020-05-11 22:31:36 -0700

    Add solution for 27-remove-element - this was very similar to the prior problem 26-remove-duplicates:

--- lc-easy/27-remove-element/solution.js
+++ lc-easy/27-remove-element/solution.js
@@ -4,5 +4,25 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+
+    if (nums.length == 0) {
+        return val;
+    }
+
+    let j = 0;
+
+    for (var i = 0; i < nums.length; ++i){
+
+        if (nums[i] == val){
+            // If the current value is NOT the target val for deletion
+            nums.splice(i,1);
+            --i;
+        } else {
+            j++;
+        }
+
+    }
+
+    return j;
     
 };
