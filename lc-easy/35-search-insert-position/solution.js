17d2d3be059b7679b145144d95e4c5f241c1ad95
Doki <jforeza@gmail.com>
2020-05-13 23:14:00 -0700

    add solution for 35-search-insert-position
    Solution checks basic corner cases/edge cases and will short terminate as soon as it finds the correct spot
     should be O(n)/space and O(n) time complexity

--- lc-easy/35-search-insert-position/solution.js
+++ lc-easy/35-search-insert-position/solution.js
@@ -5,4 +5,27 @@
  */
 var searchInsert = function(nums, target) {
     
+    const nLen = nums.length;
+    
+    // If the list is empty, `target` will be inserted at position 0
+    if (nLen == 0){
+        return 0;
+    }
+
+    // If the target number is less than the first index of nums, it'll be inserted at position 0
+    if (target < nums[0]) {
+        return 0;
+    }
+
+    // If the target number is larger than the largest index, it'll come in as the last index
+    if (target > nums[nLen-1]) {
+        return nums.length;
+    }
+
+    for (var i = 0; i< nLen; ++i) {
+        if (target <= nums[i]){
+            return i;
+        }
+    }
+    
 };
